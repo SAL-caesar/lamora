6d324dbbf11e1c15ed8e2d0621fffaa89dc0bbac
SAL-caesar <a9452167@gmail.com>
2025-11-22 07:46:16 -0800

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,8 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Navbar from "@/components/Navbar";
-import { supabase } from "@/lib/supabaseClient";
+import Navbar from "components/Navbar";
+import { supabase } from "lib/supabaseClient";
 
 export default function DashboardPage() {
   const [profile, setProfile] = useState(null);
