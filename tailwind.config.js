937cee17341c2fa9c65056d6eeeb23f6ebedc907
SAL-caesar <a9452167@gmail.com>
2025-11-22 05:48:30 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {
       colors: {
-        lamoraDark: "#050816",
-        lamoraPrimary: "#00D395",
-        lamoraSecondary: "#0F172A"
+        lamoraBlack: "#050509",
+        lamoraGold: "#f5c451",
+        lamoraGray: "#0f1015"
       }
     }
   },
